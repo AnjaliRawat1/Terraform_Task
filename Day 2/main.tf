b36b16d4f24691e5070c73d357f212c86bb093d5
Anjali <126069509+AnjaliRawat1@users.noreply.github.com>
2025-09-05 10:56:59 +0530

    Update main.tf

--- Day 2/main.tf
+++ Day 2/main.tf
@@ -1,3 +1,10 @@
+#1) The name of the security group must be devops-sg.
+#2) The description must be Security group for Nautilus App Servers.
+#3) Add an inbound rule of type HTTP, with a port range of 80, and source CIDR range 0.0.0.0/0.
+#4) Add another inbound rule of type SSH, with a port range of 22, and source CIDR range 0.0.0.0/0.
+#Ensure that the security group is created in the us-east-1 region using Terraform.
+
+
 provider "aws"  {
   region = "us-east-1"
 }
@@ -32,3 +39,4 @@ tags = {
 Name = "datacenter-sg"
 }
 }
+
