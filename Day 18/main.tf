97bfcc51441a2eacdf1755f13334aaf0eeb799ba
Anjali <126069509+AnjaliRawat1@users.noreply.github.com>
2025-09-15 19:22:46 +0530

    Update main.tf

---
+++ Day 18/main.tf
@@ -0,0 +1,25 @@
+resource "aws_kinesis_stream" "devops_stream" {
+  name                      = "devops-stream"
+  shard_count               = 1
+  retention_period          = 24
+  encryption_type           = "NONE"
+  enforce_consumer_deletion = false
+
+  stream_mode_details {
+    stream_mode = "PROVISIONED"
+  }
+
+  tags = {
+    Name = "devops-stream"
+    Env  = "Dev"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      stream_mode_details,
+      enforce_consumer_deletion,
+      tags,
+      tags_all
+    ]
+  }
+}
